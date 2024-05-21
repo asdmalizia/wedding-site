5f23b8ff7a7717fc771ea817430c8ebde10b89a6
kaiopimentel <pimentelkaio12@gmail.com>
2024-05-21 17:11:33 -0300

    proxy config

--- server.js
+++ server.js
@@ -53,12 +53,21 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.post('/proxy', async (req, res) => {
+    console.log('Received request at /proxy:', req.body);
     try {
-        const response = await axios.post(config.googleSheetUrl, req.body);
+        const response = await axios.post('https://script.google.com/macros/s/AKfycbykvZV8SbRI3rOL_tsvYNUfamxsNl2_YbgmybdtDvGAgvzwMMyg_q01k2LngcUDbIEO/exec', req.body);
+        console.log('Response from Google Sheets:', response.data);
         res.json(response.data);
     } catch (error) {
-        console.error('Error forwarding request:', error);
-        res.status(500).send('Error forwarding request');
+        console.error('Error forwarding request:', error.message);
+        if (error.response) {
+            console.error('Response data:', error.response.data);
+            console.error('Response status:', error.response.status);
+            console.error('Response headers:', error.response.headers);
+            res.status(error.response.status).send(error.response.data);
+        } else {
+            res.status(500).send('Error forwarding request');
+        }
     }
 });
 
